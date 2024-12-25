cf7122496eaa95628e80a2279a20850bd02db11f
aksharmty <aksharmty@ymail.com>
2024-12-25 14:28:18 +0530

    Add files via upload

---
+++ upiqr-js.php
@@ -0,0 +1,34 @@
+<!--img src="https://chart.scrippter.com/chart?cht=upi-qr&chs=57x57&chpa=sakhihosting%40oksbi&chpn=raj%20kumar&chta=10&chtn=test&choe=UTF-8&chld=L|1">
+<br-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generate UPI QR Code</title>
+    <!-- script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script -->
+    <script src="qrcode.min.js"></script>
+</head>
+<body>
+    <h1>UPI QR Code Generator</h1>
+    
+    <!-- Container for the QR code -->
+    <div id="qrcode"></div>
+
+    <script>
+        // UPI URL (replace with actual values)
+        var upi_url = 'upi://pay?pa=sakhihosting@oksbi&pn=Raj%20Kumar&mc=1234&tid=123456789&tr=TR12345&tn=Payment%20for%20product&am=11.00&cu=INR';
+
+        // Generate the UPI QR code using the qrcode.js library
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: upi_url, // The UPI URL
+            width: 256,    // Width of the QR code
+            height: 256,   // Height of the QR code
+            colorDark : "#000000", // Dark color
+            colorLight : "#ffffff", // Light color
+            correctLevel : QRCode.CorrectLevel.H // Error correction level (L, M, Q, H)
+        });
+    </script>
+</body>
+</html>
+
